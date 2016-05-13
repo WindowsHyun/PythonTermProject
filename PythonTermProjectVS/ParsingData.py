d44555168994fc85706306ca7b58c72a217624c4
WindowsHyun <kwon616@gmail.com>
2016-05-14 02:46:14 +0900

    Update py Code 시/도, 시/군/구, 읍/면/동 가져올수 있는 리스트를 만들었으며, 리스트에 값을 파싱할수 있게 ParsingData.py 파일을 작성 함수로 작성하여 url, key, value 를 넣으면 xml 데이터를 가져온다. xml 데이터를 addParsingDataList 에 넣어 원하는 부분을 listBox에 자동으로 추가할수 있다.

---
+++ PythonTermProjectVS/ParsingData.py
@@ -0,0 +1,44 @@
+import urllib.request
+import string
+import codecs
+from xml.dom.minidom import *
+
+#인코딩
+def urlencode(string):
+    return urllib.parse.quote(string)
+
+#디코딩
+def urldecode(string):
+    return urllib.parse.quote(string)
+
+def myIPLocation():
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')]
+    # ↑ User-Agent를 입력하지 않을경우 naver.com 에서 정상적인 접근이 아닌것으로 판단하여 차단을 한다.
+    data = ""
+    with opener.open('http://map.naver.com') as f:
+        data = f.read(300000).decode('utf-8') # 300000bytes 를 utf-8로 변환하여 읽어온다.  변환이 없을경우 unicode로 받아온다.
+    
+    p1 = data.find("<span class=\"blind\">") + 20 # 현재 위치를 나타내 주는 글자의 위치를 구한다.
+    data = data[p1:p1 + 50] # 해당 글자 위치에서 50글자를 data에 넣는다.
+    p2 = data.find("</span>")
+
+    return data[:p2]
+
+def openAPItoXML(server, key, value):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')]
+    # ↑ User-Agent를 입력하지 않을경우 naver.com 에서 정상적인 접근이 아닌것으로 판단하여 차단을 한다.
+    data = ""
+    urldata = server + key + value
+    with opener.open(urldata) as f:
+        data = f.read(300000).decode('utf-8') # 300000bytes 를 utf-8로 변환하여 읽어온다.  변환이 없을경우 unicode로 받아온다.
+    return data
+
+def addParsingDataList(xmlData, motherData, childData, addList):
+    doc = parseString(xmlData)
+    siGunGuList = doc.getElementsByTagName(motherData)
+    signguCdSize = len(siGunGuList)
+    for index in range(signguCdSize):
+        mphms = siGunGuList[index].getElementsByTagName(childData)
+        addList.addItem(str(mphms[0].firstChild.data))
--- PythonTermProjectVS/PythonTermProjectVS.py
+++ PythonTermProjectVS/PythonTermProjectVS.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 import sys
+import urllib
+
 import GUI
+from ParsingData import *
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QGridLayout, QLabel, QLineEdit
-from PyQt5.QtWidgets import QTextEdit, QWidget, QDialog, QApplication, QMessageBox
+from PyQt5.QtWidgets import QTextEdit, QWidget, QDialog, QApplication, QMessageBox, QPushButton, QMainWindow
 
 class MainWindow(QDialog, GUI.Ui_Dialog):
     def closeEvent(self, event):
@@ -13,25 +17,63 @@ class MainWindow(QDialog, GUI.Ui_Dialog):
             event.accept() 
         else:
             event.ignore()      
+    
+    def ListClicked(self):
+        sender = self.sender()
+        list = sender.currentItem()
+        serverurl = "http://openapi.epost.go.kr/postal/retrieveLotNumberAdressService/retrieveLotNumberAdressService/getEupMyunDongList?ServiceKey="
+        serverkey = "agRTEvpQv1bNvtoPQr3DNvE5juZ9EAws47JkmLbQnf4OYYAXw%2FAh9TULJtGxrEBzqH2767koxGlukyRTjweQcg%3D%3D"
+        servervalue = "&brtcCd=" + urlencode(self.LocationData) + "&signguCd=" + urlencode(list.text()) +  "&numOfRows=999&pageSize=999&pageNo=1&startPage=1"
+        self.DetailData = list.text()
+        self.DetailList.clear()
+        areaData = openAPItoXML(serverurl, serverkey, servervalue)
+        addParsingDataList(areaData, "eupMyunDongList", "emdCd", self.DetailList)
+
+
+    def BtnClicked(self):
+        sender = self.sender()
+        if sender.text() == "검색":
+            serverurl = "http://openapi.epost.go.kr/postal/retrieveLotNumberAdressService/retrieveLotNumberAdressService/getSiGunGuList?ServiceKey="
+            serverkey = "agRTEvpQv1bNvtoPQr3DNvE5juZ9EAws47JkmLbQnf4OYYAXw%2FAh9TULJtGxrEBzqH2767koxGlukyRTjweQcg%3D%3D"
+            servervalue = "&brtcCd="+ urlencode(self.LocationBox.currentText()) +  "&numOfRows=999&pageSize=999&pageNo=1&startPage=1"
+            self.LocationData = self.LocationBox.currentText()
+            self.LocationList.clear()
+            areaData = openAPItoXML(serverurl, serverkey, servervalue)
+            addParsingDataList(areaData, "siGunGuList", "signguCd", self.LocationList)
+
+        if sender.text() == "저장":
+            if self.DetailList.currentRow() == -1:
+                QMessageBox.information(self, sender.text() , "동/읍/면 선택해주세요..!", QMessageBox.Yes)
+            else:
+                QMessageBox.information(self, sender.text() , "%s %s %s 맞나요?" % (self.LocationData, self.DetailData, self.DetailList.currentItem().text()), QMessageBox.Yes)
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+        self.myLocationData = myIPLocation() # 내위치를 가져오기
+        self.LocationData = "" # 서울시 처럼 데이터를 기억하기
+        self.DetailData = "" # 읍,면,동 데이터를 기억하기
         self.setupUi(self)
         self.dust_icon.setPixmap(QPixmap('./Img/dust_icon.png')) # fore_icon 관련 이미지 추가
         self.fore_icon.setPixmap(QPixmap('./Img/fore_icon.png')) # fore_icon 관련 이미지 추가
         self.GPSImage.setPixmap(QPixmap('./Img/station_icon.png')) # GPS 관련 이미지 추가
         self.stateImg.setPixmap(QPixmap('./Img/state_img.png')) # 상태 관련 이미지 추가
-        koreaArea = ["서울특별시", "경기도", "인천광역시", "강원도", "충청남도", "대전광역시",
-                          "충청북도", "경상북도", "대구광역시", "전라북도", "광주광역시", "전라남도",
-                          "경상남도", "울산광역시", "부산광역시", "제주도" ]
+        koreaArea = ["서울", "강원", "인천", "경기", "충북", "충남",
+                          "경북", "대전", "대구", "전북", "경남", "울산",
+                          "광주", "부산", "전남", "제주" ]
         for data in koreaArea: 
             self.LocationBox.addItem(data)
 
+        self.searchBtn.clicked.connect(self.BtnClicked)
+        self.LocationList.clicked.connect(self.ListClicked)
+        self.saveBtn.clicked.connect(self.BtnClicked)
+        self.Loaction.setText(self.myLocationData)
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     form = MainWindow()
     form.show()
-
     sys.exit(app.exec_())
+
+
+
