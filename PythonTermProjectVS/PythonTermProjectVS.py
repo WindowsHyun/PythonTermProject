18fd7208b8a001d3f665eea0b4da5c28e9fd222b
WindowsHyun <kwon616@gmail.com>
2016-05-14 21:18:54 +0900

    Update py Code, Upload image 아이콘 이미지 추가, pm10 관련 이미지 임시 추가 미세먼지 세부사항 추가, 오염정보 관련해서 시간당, 24시간당 두개를 추가함.

--- PythonTermProjectVS/PythonTermProjectVS.py
+++ PythonTermProjectVS/PythonTermProjectVS.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import urllib
+import time
 
 import GUI
 from ParsingData import *
@@ -75,13 +76,15 @@ class MainWindow(QDialog, GUI.Ui_Dialog):
 
         if sender.text() == "갱신":
             serverurl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?ServiceKey="
-            servervalue = "&stationName=" + urlencode(self.DetailListData) + "&numOfRows=1&pageSize=1&pageNo=1&startPage=1&dataTerm=DAILY"
+            servervalue = "&stationName=" + urlencode(self.DetailListData) + "&dataTerm=month&pageNo=1&numOfRows=10&ver=1.2&"
             areaData = openAPItoXML(serverurl, self.serverKey, servervalue)
             self.getTime.setText("측정 시간 : %s" % addParsingDataString(areaData, "item", "dataTime"))
             self.totalLabel.setText(addParsingDataString(areaData, "item", "khaiValue"))
             self.totalLabel.setStyleSheet('color: rgb(255, 0, 0);')
-            self.pm10Label.setText(addParsingDataString(areaData, "item", "pm10Value"))
+            self.pm10Label.setText(addParsingDataString(areaData, "item", "pm10Value24"))
             self.pm10Label.setStyleSheet('color: rgb(207, 78, 78);')
+            self.pm10_1hLabel.setText("시간당 : " + str(addParsingDataString(areaData, "item", "pm10Value")) + "㎍/㎥")
+            self.pm10_1hLabel.setStyleSheet('color: rgb(207, 78, 78);')
             self.o3Label.setText(addParsingDataString(areaData, "item", "o3Value"))
             self.o3Label.setStyleSheet('color: rgb(0, 162, 232);')
             self.No2Label.setText(addParsingDataString(areaData, "item", "no2Value"))
@@ -90,6 +93,24 @@ class MainWindow(QDialog, GUI.Ui_Dialog):
             self.CoLabel.setStyleSheet('color: rgb(34, 177, 76);')
             self.So2Label.setText(addParsingDataString(areaData, "item", "so2Value"))
             self.So2Label.setStyleSheet('color: rgb(0, 162, 232);')
+            QMessageBox.information(self, sender.text() , "갱신을 완료하였습니다..!",QMessageBox.Yes)
+
+        if sender.text() == "Refresh":
+            now = time.localtime()
+            dmon = now.tm_mon
+            dday = now.tm_mday
+            if now.tm_mon < 10 : dmon = str("0" + str(now.tm_mon))
+            if now.tm_mday < 10 : dday = str("0" + str(now.tm_mday))
+            nowdate = str(now.tm_year) + "-" + str(dmon) + "-" + str(dday)
+
+            serverurl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth?ServiceKey="
+            servervalue = "&searchDate=" + urlencode(nowdate) + ""
+            areaData = openAPItoXML(serverurl, self.serverKey, servervalue)
+            
+            self.today_date.setText("%s" % addParsingDataString(areaData, "item", "dataTime"))
+            self.informOverall.setPlainText("%s" % addParsingDataString(areaData, "item", "informOverall"))
+            self.informCause.setPlainText("%s" % addParsingDataString(areaData, "item", "informCause"))
+            self.informGrade.setPlainText("%s" % addParsingDataString(areaData, "item", "informGrade").replace(",", ", "))
             QMessageBox.information(self, sender.text() , "갱신을 완료하였습니다..!",QMessageBox.Yes)
             pass
 
@@ -107,6 +128,7 @@ class MainWindow(QDialog, GUI.Ui_Dialog):
         self.fore_icon.setPixmap(QPixmap('./Img/fore_icon.png')) # fore_icon 관련 이미지 추가
         self.GPSImage.setPixmap(QPixmap('./Img/station_icon.png')) # GPS 관련 이미지 추가
         self.stateImg.setPixmap(QPixmap('./Img/state_img.png')) # 상태 관련 이미지 추가
+        self.pm10_img.setPixmap(QPixmap('./Img/pm10_Test.png')) # pm10 관련 이미지
         koreaArea = ["서울", "강원", "인천", "경기", "충북", "충남",
                           "경북", "대전", "대구", "전북", "경남", "울산",
                           "광주", "부산", "전남", "제주"]
@@ -118,12 +140,14 @@ class MainWindow(QDialog, GUI.Ui_Dialog):
         self.searchBtn_2.clicked.connect(self.BtnClicked)
         self.saveBtn.clicked.connect(self.BtnClicked)
         self.refreshBtn.clicked.connect(self.BtnClicked)
+        self.refreshBtn_2.clicked.connect(self.BtnClicked)
         self.Loaction.setText(self.myLocationData)
         
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = MainWindow()
+    form.setWindowIcon(QIcon('./Img/main_icon.png'))
     form.show()
     sys.exit(app.exec_())
 
